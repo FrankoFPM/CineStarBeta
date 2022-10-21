bc5d367e90de599b1d6125311878c8f4110b6081
Franco <frank.13.gp@gmail.com>
2022-10-21 16:20:48 -0500

    ImgCines

---
+++ src/img/cine/ImgCines.js
@@ -0,0 +1,66 @@
+import img11 from './1.1.jpg'
+import img12 from './1.2.jpg'
+import img13 from './1.3.jpg'
+import img21 from './2.1.jpg'
+import img22 from './2.2.jpg'
+import img23 from './2.3.jpg'
+import img31 from './3.1.jpg'
+import img32 from './3.2.jpg'
+import img33 from './3.3.jpg'
+import img41 from './4.1.jpg'
+import img42 from './4.2.jpg'
+import img43 from './4.3.jpg'
+import img51 from './5.1.jpg'
+import img52 from './5.2.jpg'
+import img53 from './5.3.jpg'
+import img61 from './6.1.jpg'
+import img62 from './6.2.jpg'
+// import img63 from './6.3.jpg'
+import img71 from './7.1.jpg'
+import img72 from './7.2.jpg'
+// import img73 from './7.3.jpg'
+import img81 from './8.1.jpg'
+import img82 from './8.2.jpg'
+// import img83 from './8.3.jpg'
+import img91 from './9.1.jpg'
+import img92 from './9.2.jpg'
+// import img93 from './9.3.jpg'
+
+export default [
+    {
+        "img1": img11,
+        "img2": img12
+    },
+    {
+        "img1": img21,
+        "img2": img22
+    },
+    {
+        "img1": img31,
+        "img2": img32
+    },
+    {
+        "img1": img41,
+        "img2": img42
+    },
+    {
+        "img1": img51,
+        "img2": img52
+    },
+    {
+        "img1": img61,
+        "img2": img62
+    },
+    {
+        "img1": img71,
+        "img2": img72
+    },
+    {
+        "img1": img81,
+        "img2": img82
+    },
+    {
+        "img1": img91,
+        "img2": img92
+    }
+]
